51497fb4a6fa259dcd71f03f33cd24234dcc89f5
Lợi Nguyễn <102460321+loinguyenvan274@users.noreply.github.com>
2022-10-18 05:41:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetchUrl = require("fetch").fetchUrl;
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 42100;
 
 const account = '@loinguyenvan274';
 
